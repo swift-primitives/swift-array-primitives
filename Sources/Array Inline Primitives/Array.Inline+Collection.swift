927ca6a4ab1fa89cf53cdb64d556e1de65d189ed
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-23 18:53:53 +0100

    Refactor Array.Inline file organization
    Split conformances into dedicated files:
    - Array.Inline+Sequence.swift: Iterator and Sequence.Protocol
    - Array.Inline+Collection.swift: Collection conformances (merged from +Collection.Indexed.swift)
    - Array.Inline.swift: API-only placeholder

--- Sources/Array Inline Primitives/Array.Inline+Collection.swift
+++ Sources/Array Inline Primitives/Array.Inline+Collection.swift
@@ -2,6 +2,17 @@ public import Collection_Primitives
 public import Index_Primitives
 public import Array_Primitives_Core
 
+// MARK: - Collection.Protocol Conformance
+// Note: Index, startIndex, endIndex, index(after:) defined in Collection.Indexed conformance
+
+extension Array.Inline: Collection.`Protocol` {}
+
+// MARK: - Collection.Access.Random Conformance
+// Note: Collection.Bidirectional conformance is provided below
+// for ALL element types (including ~Copyable) via `where Element: ~Copyable`.
+
+extension Array.Inline: Collection.Access.Random {}
+
 // MARK: - Collection.Indexed Conformance
 
 extension Array.Inline: Collection.Indexed where Element: ~Copyable {
@@ -23,3 +34,6 @@ extension Array.Inline: Collection.Bidirectional where Element: ~Copyable {
     @inlinable
     public func index(before i: Index) -> Index { (i - 1)! }
 }
+
+// Note: Array.Inline cannot conform to Swift.Collection because it is unconditionally
+// ~Copyable (has deinit for inline storage cleanup). Swift.Collection requires Self: Copyable.
